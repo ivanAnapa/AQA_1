39a4efbcd80fd280da69894ab7e31514349cf508
ivanAnapa <vanya-cherniy@mail.ru>
2022-07-25 23:12:13 +0300

    testNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,22 +9,25 @@ public class CashbackHackServiceTest {
 
     @Test
     public void remainWhenAmountIs900Test() {
+        int amount = 900;
         int expected = 100;
-        int actual = service.remain(900);
-        Assert.assertEquals(actual, expected);
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected, "Wrong remain with amount " + amount + ": ");
     }
 
     @Test
     public void remainWhenAmountIs1100Test() {
+        int amount = 1100;
         int expected = 900;
-        int actual = service.remain(1100);
-        Assert.assertEquals(actual, expected);
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected, "Wrong remain with amount " + amount + ": ");
     }
 
     @Test
     public void remainWhenAmountIs1000Test() {
+        int amount = 1000;
         int expected = 0;
-        int actual = service.remain(1000);
-        Assert.assertEquals(actual, expected);
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected, "Wrong remain with amount " + amount + ": ");
     }
 }
